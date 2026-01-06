e003ba3c27dd44e2e6dc7600faaa89df8e3a6d2e
hari anitha <hv8447@gmail.com>
2026-01-06 22:02:18 +0530

    Proper layout provided for the /app/home route page

--- src/app/app/home/page.tsx
+++ src/app/app/home/page.tsx
@@ -25,18 +25,30 @@ export default function FeedsPage() {
 
 
     return <div className={styles.main_container}>
-        <MainContainerHeader header="Feed sources" count={feedSources.count}><div></div></MainContainerHeader>
-        {feedSources.feeds.map((feedSource: any) => {
-            const domainUrl = getDomainFromUrl(feedSource.url)
-            return (
-                <div className={styles.feedSource} key={feedSource.id}>
-                    {feedSource.favicon ? <img className={styles.feed_source_img} src={feedSource.favicon} alt={`${feedSource.title}-icon`} /> : <div className={styles.feed_source_icon}>{getFeedSourceInitials(feedSource.title)}</div>}
-                    <h2 className={styles.title}>{feedSource.title}</h2>
-                    <dd className={styles.description}>{feedSource.description}</dd>
-                    <a className={styles.rss_link} href={`https://${domainUrl}`} target="_blank">{domainUrl} </a>
-                </div>
-            )
-        })}
-        <p className="no_data_message">No more data available</p>
+        <div className={styles.top_occupier}>
+            <div className={styles.top_right_portion} />
+            <div className={styles.top_left_portion} />
+        </div>
+        <div className={styles.header}>
+            <MainContainerHeader header="Feed sources" count={feedSources.count}><div></div></MainContainerHeader>
+            <div className={styles.search_container}></div>
+        </div>
+        <div className={styles.left_section}>
+            <div className={styles.feed_list}>
+                {feedSources.feeds.map((feedSource: any) => {
+                    const domainUrl = getDomainFromUrl(feedSource.url)
+                    return (
+                        <div className={styles.feedSource} key={feedSource.id}>
+                            {feedSource.favicon ? <img className={styles.feed_source_img} src={feedSource.favicon} alt={`${feedSource.title}-icon`} /> : <div className={styles.feed_source_icon}>{getFeedSourceInitials(feedSource.title)}</div>}
+                            <h2 className={styles.title}>{feedSource.title}</h2>
+                            <dd className={styles.description}>{feedSource.description}</dd>
+                            <a className={styles.rss_link} href={`https://${domainUrl}`} target="_blank">{domainUrl} </a>
+                        </div>
+                    )
+                })}
+                <p className="no_data_message">No more data available</p>
+            </div>
+            <div className={styles.suggestions_container}></div>
+        </div>
     </div>
 }
